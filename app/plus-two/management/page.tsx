0c97cf47e0b97bdd4786ce232c1dd28db804f3df
sujan-py <sujanmakalu@gmail.com>
2026-03-08 14:42:40 +0545

    fulld code

---
+++ app/plus-two/management/page.tsx
@@ -0,0 +1,8 @@
+export default function PlusTwoManagementPage() {
+  return (
+    <section style={{ padding: "6rem 5% 3rem" }}>
+      <h1>+2 Management</h1>
+      <p>Program details will be updated soon.</p>
+    </section>
+  );
+}
