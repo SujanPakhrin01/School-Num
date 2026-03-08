dff4572c2b53788771c3511d17bacdc19993a1a2
sujan-py <sujanmakalu@gmail.com>
2026-03-08 14:37:18 +0545

    all

---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+    <section style={{ padding: "6rem 5% 3rem" }}>
+      <h1>Contact</h1>
+      <p>Please contact the school office for more information.</p>
+    </section>
+  );
+}
