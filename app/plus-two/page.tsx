b99f4f4b2d6f3b5aad0216aa0df54e1137e9897e
sujan-py <sujanmakalu@gmail.com>
2026-03-08 14:49:15 +0545

    push all code

---
+++ app/plus-two/page.tsx
@@ -0,0 +1,8 @@
+export default function PlusTwoPage() {
+  return (
+    <section style={{ padding: "6rem 5% 3rem" }}>
+      <h1>+2 Programmes</h1>
+      <p>Select a stream to view details.</p>
+    </section>
+  );
+}
